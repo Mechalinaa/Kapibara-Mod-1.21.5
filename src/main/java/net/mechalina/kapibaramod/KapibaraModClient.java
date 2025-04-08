0d990e7527dd77c84324c3d421e8259c604ee2ca
Mechalinaa <lenska.michalina@gmail.com>
2025-04-08 23:04:39 +0200

    Initial commit

---
+++ src/main/java/net/mechalina/kapibaramod/KapibaraModClient.java
@@ -0,0 +1,10 @@
+package net.mechalina.kapibaramod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KapibaraModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mechalina/kapibaramod/KapibaraModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mechalina.kapibaramod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KapibaraModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
