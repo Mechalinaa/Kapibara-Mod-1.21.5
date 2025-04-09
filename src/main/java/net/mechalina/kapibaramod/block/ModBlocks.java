b218bb9e25319dc07b6db297a23d09b7722b3609
Mechalinaa <lenska.michalina@gmail.com>
2025-04-09 23:53:54 +0200

    Adding orange fruit and orange wood block

---
+++ src/main/java/net/mechalina/kapibaramod/block/ModBlocks.java
@@ -0,0 +1,43 @@
+package net.mechalina.kapibaramod.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.mechalina.kapibaramod.KapibaraMod;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block ORANGE_WOOD_BLOCK = registerBlock("orange_wood_block",
+            AbstractBlock.Settings.create().strength(4f).sounds(BlockSoundGroup.WOOD));
+
+    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings){
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(KapibaraMod.MOD_ID, name));
+        Block block = new Block(blockSettings.registryKey(key));
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, key, block);
+    }
+
+    private static void registerBlockItem(String name, Block block){
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(KapibaraMod.MOD_ID, name));
+        BlockItem item = new BlockItem(block, new Item.Settings().registryKey(key));
+        Registry.register(Registries.ITEM, key, item);
+
+    }
+    public static void registerModBlocks(){
+        KapibaraMod.LOGGER.info("Registering Mod Blocks for "+ KapibaraMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries ->{
+            entries.add(ModBlocks.ORANGE_WOOD_BLOCK);
+        });
+    }
+
+}
---
+++ src/main/java/net/mechalina/kapibaramod/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.mechalina.kapibaramod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.mechalina.kapibaramod.KapibaraMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item ORANGE=registerItem("orange",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,
+            Identifier.of(KapibaraMod.MOD_ID,"orange")))));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(KapibaraMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        KapibaraMod.LOGGER.info("Registering Mod Items for "+KapibaraMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries ->{
+            entries.add(ORANGE);
+        });
+
+    }
+}
